33242fe117ab1bea4e370a147fac6b85ef4a1dd9
CROS <orej17rolando@gmail.com>
2022-08-15 00:07:52 -0500

    status error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routerApi = require('./routes')
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('estamos en express');
 })
--- routes/products.router.js
+++ routes/products.router.js
@@ -3,6 +3,7 @@ const faker = require("faker");
 
 const router = express.Router();
 
+
 router.get('/',(req,res) =>{
 
   res.json([
@@ -45,5 +46,31 @@ router.get('/:id',(req,res)=>{
     }
   )
 })
+
+router.post('/', (req,res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body
+  });
+});
+
+router.patch('/:id', (req,res) => {
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id
+  });
+});
+
+router.delete('/:id', (req,res) => {
+  const {id} = req.params;
+  res.json({
+    message: 'delete',
+    id
+  });
+});
 
 module.exports = router;
